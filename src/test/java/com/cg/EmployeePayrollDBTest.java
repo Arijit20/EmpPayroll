d0f80783ea0501a95d2d75e0ef29dbcf9806c634
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-29 00:25:07 +0530

    [Arijit]Added : Test Case For Updatation And Sync

--- src/test/java/com/cg/EmployeePayrollDBTest.java
+++ src/test/java/com/cg/EmployeePayrollDBTest.java
@@ -8,11 +8,22 @@ import org.junit.Test;
 import com.cg.EmpPayrollService.IOService;
 
 public class EmployeePayrollDBTest {
+	
+	
 
 	@Test
 	public void givenEmployeePayrollDB_shouldReturnCount() throws EmpPayrollException {
 		EmpPayrollService empPayRollService = new EmpPayrollService();
 		List<EmployeePayrollData> empPayrollList = empPayRollService.readEmpPayrollData(IOService.DB_IO);
 		Assert.assertEquals(3, empPayrollList.size());
+	}
+	
+	@Test
+	public void givenNewSalary_whenUpdatedShouldMatch() throws EmpPayrollException {
+		EmpPayrollService empPayRollService = new EmpPayrollService();
+		List<EmployeePayrollData> empPayrollList = empPayRollService.readEmpPayrollData(IOService.DB_IO);
+		empPayRollService.updateEmployeeSalary("Bill", 150.00);
+		boolean result = empPayRollService.checkEmployeePayrollInSyncWithDB("Bill");
+		Assert.assertTrue(result);
 	}
 }
