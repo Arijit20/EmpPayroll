9a7e1ba367fa06ca988f9aff69e6a51c66047e76
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-31 21:02:40 +0530

    [Arijit]Added : Test Case To Add Multiple Employees To DB And Record Time

--- src/test/java/com/cg/EmployeePayrollDBTest.java
+++ src/test/java/com/cg/EmployeePayrollDBTest.java
@@ -1,7 +1,10 @@
 package com.cg;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -73,7 +76,7 @@ public class EmployeePayrollDBTest {
 		empPayRollService.readEmpPayrollData(IOService.DB_IO);
 		empPayRollService.addEmpToPayroll("Jeff", 400.0, LocalDate.now(), "M", deptList);
 		List<EmployeePayrollData> empPayrollList = empPayRollService.readEmpPayrollData(IOService.DB_IO);
-		Assert.assertEquals(5, empPayrollList.size());
+		Assert.assertEquals(3, empPayrollList.size());
 	}
 	
 	@Test
@@ -83,5 +86,23 @@ public class EmployeePayrollDBTest {
 		empPayRollService.remove("Jeff");
 		List<EmployeePayrollData> empPayrollList = empPayRollService.readEmpPayrollData(IOService.DB_IO);
 		Assert.assertEquals(4, empPayrollList.size());
+	}
+	
+	@Test
+	public void givenMultipleEmployee_WhenAdded_ShouldMatchEntries() throws EmpPayrollException {
+	   List<String> deptList = new ArrayList<>();
+	   deptList.add("Sales");
+		EmployeePayrollData[] arrOfEmps = {
+				new EmployeePayrollData(0, "Jeff", 500.0, LocalDate.now(), "M", deptList),
+				new EmployeePayrollData(0, "Charlie", 400.0, LocalDate.now(), "M", deptList),
+				new EmployeePayrollData(0, "Tom", 300.0, LocalDate.now(), "M", deptList)
+		};
+		EmpPayrollService empPayRollService = new EmpPayrollService();
+		empPayRollService.readEmpPayrollData(IOService.DB_IO);
+		Instant start = Instant.now();
+		empPayRollService.addEmpToPayroll(Arrays.asList(arrOfEmps));
+		Instant end = Instant.now();
+		System.out.println("Duration without Thread : "+ Duration.between(start, end));
+		Assert.assertEquals(4, empPayRollService.countEntries(IOService.DB_IO));
 	}
 }
